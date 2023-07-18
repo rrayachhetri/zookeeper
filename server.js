5ddb0cc66067f710d02c3205b96e7ac44a0088bd
rrayachhetri <rrayachhetri@gmail.com>
2023-07-17 23:40:56 -0700

    PORT correction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
