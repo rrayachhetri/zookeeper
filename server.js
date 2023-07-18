7016b2df2f841383f3bd3aa99af2714a4f4e722f
rrayachhetri <rrayachhetri@gmail.com>
2023-07-17 23:30:44 -0700

    heroku setup

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const { animals } = require("./data/animals");
 
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.listen(3001, () => {
-  console.log("API server now on port 3001");
+  console.log(`API server now on port ${PORT}!`);
 });
 
 app.get("/api/animals", (req, res) => {
