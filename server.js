b1e24aabc5716c8c6559427e2668a864543d4f03
rrayachhetri <rrayachhetri@gmail.com>
2023-07-17 23:14:37 -0700

    handled request parameters for animals

--- server.js
+++ server.js
@@ -7,29 +7,6 @@ app.listen(3001, () => {
   console.log("API server now on port 3001");
 });
 
-function filterByQuery(query, animalsArray) {
-  let filterdResult = animalsArray;
-  if (query.diet) {
-    filterdResult = filterdResult.filter(
-      (animal) => animal.diet === query.diet
-    );
-  }
-
-  if (query.species) {
-    filterdResult = filterdResult.filter(
-      (animal) => animal.species === query.species
-    );
-  }
-
-  if (query.name) {
-    filterdResult = filterdResult.filter(
-      (animal) => animal.name === query.name
-    );
-  }
-
-  return filterdResult;
-}
-
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -39,3 +16,53 @@ app.get("/api/animals", (req, res) => {
 
   res.json(results);
 });
+
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+// Note that we save the animalsArray as filteredResults
+  let filteredResult = animalsArray;
+  if (query.personalityTraits) {
+    // Save personalityTraits as a dedicated array
+    // If personalityTraits is a string, place it into a new array and save
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // loop through each traits in the personalityTraits array
+    personalityTraitsArray.forEach(trait => {
+      // Check the trait against each animal in the filteredResults array.
+      // Remember, it is initially a copy of the animalsArray
+      // but here we're updating it for each trait in the .forEach() loop.
+      // For each trait being targeted by filter, the filteredResults
+      // array will then contain only the enteries that contain the trait,
+      // so at the end we'll have an array of animals that have every one
+      // of the traits when .forEach() loop is finished.
+      filteredResult = filteredResult.filter
+        (animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+
+  if (query.diet) {
+    filteredResult = filteredResult.filter
+      (animal => animal.diet === query.diet
+    );
+  }
+
+  if (query.species) {
+    filteredResult = filteredResult.filter
+      (animal => animal.species === query.species
+    );
+  }
+
+  if (query.name) {
+    filteredResult = filteredResult.filter
+      (animal => animal.name === query.name
+    );
+  }
+
+  return filteredResult;
+}
+
